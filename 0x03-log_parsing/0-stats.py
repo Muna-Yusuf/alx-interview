786bb331370297dbbba08f123f4cf643c0c5a2ba
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-18 11:25:33 +0000

    log_parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line, parses the log format"""
+import sys
+import signal
+
+total_size = 0
+status_counts = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0}
+
+
+def print_stats():
+    """function prints the current stats"""
+    print(f"File size: {total_size}")
+    for code in sorted(status_counts):
+        if status_counts[code] > 0:
+            print(f"{code}: {status_counts[code]}")
+
+
+def signal_handler(sig, frame):
+    """to handel the signal"""
+    print_stats()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+line_count = 0
+
+for line in sys.stdin:
+    parts = line.split()
+    if len(parts) > 6:
+        try:
+            size = int(parts[-1])
+            code = int(parts[-2])
+            total_size += size
+            if code in status_counts:
+                status_counts[code] += 1
+        except:
+            continue
+    line_count += 1
+    if line_count % 10 == 0:
+        print_stats()
+
+print_stats()
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,10 @@
+#  0x03. Log Parsing
+
+<p align="center"> <img src="https://i1.wp.com/www.learnsteps.com/wp-content/uploads/2019/10/logparsing.png" width="550" higth="550">
+
+## Description:
+
+This repository contains a Python script designed to parse log entries from standard input, 
+compute specific metrics, and print statistics. The script processes logs line by line, following a predefined format, 
+and outputs cumulative statistics every 10 lines or upon receiving a keyboard interruption (Ctrl + C).
+
