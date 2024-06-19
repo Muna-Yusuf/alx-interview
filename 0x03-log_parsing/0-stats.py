a9d481dd0fbff3dc1b4616b98996cff9c4fac3e0
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-19 09:17:53 +0000

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,61 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line, parses the log format"""
 import sys
 import signal
 
-total_size = 0
+# Dictionary to hold the count of status codes
 status_counts = {
         200: 0,
+        301: 0,
+        400: 0,
         401: 0,
         403: 0,
         404: 0,
         405: 0,
-        500: 0
-        }
+        500: 0}
+total_file_size = 0
+line_count = 0
 
 
 def print_stats():
-    """function prints the current stats"""
-    print(f"File size: {total_size}")
-    for code in sorted(status_counts):
-        if status_counts[code] > 0:
-            print(f"{code}: {status_counts[code]}")
+    global total_file_size, status_counts
+    print(f"File size: {total_file_size}")
+    for status_code in sorted(status_counts.keys()):
+        if status_counts[status_code] > 0:
+            print(f"{status_code}: {status_counts[status_code]}")
 
 
 def signal_handler(sig, frame):
-    """to handel the signal"""
     print_stats()
     sys.exit(0)
 
 
+# Register the signal handler for keyboard interruption
 signal.signal(signal.SIGINT, signal_handler)
 
-line_count = 0
+try:
+    for line in sys.stdin:
+        parts = line.split()
 
-for line in sys.stdin:
-    parts = line.split()
-    if len(parts) > 6:
-        try:
-            size = int(parts[-1])
-            code = int(parts[-2])
-            total_size += size
-            if code in status_counts:
-                status_counts[code] += 1
-        except ValueError:
+        # Check if the line format is correct
+        if len(parts) != 9 or parts[5] != '"GET'
+        or parts[6] != '/projects/260' or parts[7] != 'HTTP/1.1"':
             continue
-    line_count += 1
-    if line_count % 10 == 0:
-        print_stats()
+        try:
+            status_code = int(parts[8])
+            file_size = int(parts[9])
+        except (ValueError, IndexError):
+            continue
 
-print_stats()
+        # Update metrics
+        if status_code in status_counts:
+            status_counts[status_code] += 1
+        total_file_size += file_size
+        line_count += 1
+
+        # Print stats every 10 lines
+        if line_count % 10 == 0:
+            print_stats()
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    print_stats()
