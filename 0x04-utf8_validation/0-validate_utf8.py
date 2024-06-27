61803d19ac8fdfe104e35e4234b8fc67efa0cc0d
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-27 18:03:38 +0000

    utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,22 +4,23 @@
 
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding."""
-    num_bytes = 0
+    nbytes = 0
 
-    for num in data:
-        byte = num & 0xFF
-        if num_bytes == 0:
-            if byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            elif byte >> 7:
+    b1 = 1 << 7
+    b2 = 1 << 6
+
+    for i in data:
+        b = 1 << 7
+        if nbytes == 0:
+            while b & i:
+                nbytes += 1
+                b = b >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if not (i & b1 and not (i & b2)):
                 return False
-        num_bytes -= 1
-
-    return num_bytes == 0
+        nbytes -= 1
+    return nbytes == 0
