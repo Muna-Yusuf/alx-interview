60d1e71107298c3b93aca623e0985fe11cfc2607
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-07-03 10:34:29 +0000

    0x05-nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def solve_nqueens_util(board, col, solutions):
+    if col >= len(board):
+        solutions.append([[i, board[i].index(1)] for i in range(len(board))])
+        return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, solutions) or res
+            board[i][col] = 0
+    return res
+
+def solve_nqueens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solve_nqueens_util(board, 0, solutions)
+    return solutions
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
+
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,8 @@
+# 0x05. N Queens
+
+<p align="center"> <img src="https://journaldev.nyc3.cdn.digitaloceanspaces.com/2020/08/queen-8.png" width="550" higth="550">
+
+## Description:
+This repository contains a Python implementation of a program to solve the N Queens puzzle. 
+The N Queens puzzle is a classic challenge where the goal is to place N non-attacking queens on an N×N chessboard.
+The program uses a backtracking algorithm to find all possible solutions for a given N.
