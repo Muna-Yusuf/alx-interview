a4cc64e6b94ad68b2a3dd89f595b23e38893fe2c
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-07-03 10:45:49 +0000

    0x05-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""N queens"""
 import sys
 
+
 def is_safe(board, row, col):
+    """Check if there's a queen in the same row to the left"""
     for i in range(col):
         if board[row][i] == 1:
             return False
@@ -14,7 +16,9 @@ def is_safe(board, row, col):
             return False
     return True
 
+
 def solve_nqueens_util(board, col, solutions):
+    """If all queens are placed, add the solution"""
     if col >= len(board):
         solutions.append([[i, board[i].index(1)] for i in range(len(board))])
         return True
@@ -26,28 +30,28 @@ def solve_nqueens_util(board, col, solutions):
             board[i][col] = 0
     return res
 
+
 def solve_nqueens(n):
+    """Initialize the board with all zeros"""
     board = [[0 for _ in range(n)] for _ in range(n)]
     solutions = []
     solve_nqueens_util(board, 0, solutions)
     return solutions
 
+
 if __name__ == "__main__":
+    """Check if the number of arguments is correct"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
     solutions = solve_nqueens(N)
     for solution in solutions:
         print(solution)
-
