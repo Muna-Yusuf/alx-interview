3fb8fdc09979dbfe146aa7445989b3c9e8bff225
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-07-25 16:18:40 +0000

    test

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Coin Change Algorithm
+"""
+
+def make_change(coin_values, target_amount):
+    """
+    Calculate the minimum number of coins needed to reach the target amount.
+    
+    Args:
+        coin_values (list): Available coin denominations.
+        target_amount (int): The desired total amount.
+    
+    Returns:
+        int: Fewest number of coins needed, or -1 if not possible.
+    """
+    if target_amount <= 0:
+        return 0
+
+    coin_values.sort(reverse=True)
+    remaining_amount, num_coins = target_amount, 0
+
+    for coin in coin_values:
+        while remaining_amount >= coin:
+            remaining_amount -= coin
+            num_coins += 1
+
+    return num_coins if remaining_amount == 0 else -1
